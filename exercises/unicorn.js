7e5dca2a2ab32a70503216fc2119796beb13d55d
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2023-07-22 17:05:15 -0600

    Add files via upload

---
+++ exercises/unicorn.js
@@ -0,0 +1,15 @@
+class Unicorn {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color || 'white';
+  }
+  isWhite() {
+    return this.color === 'white'
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`
+  }
+}
+
+module.exports = Unicorn;
